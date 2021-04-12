9bebb2cbc1e343edd97820cc8c20ebf00209976d
gainggo <gainggo@naver.com>
2021-04-12 22:40:53 +0900

    0412 2240

--- java/com/project/UsERP/controller/AndroidController.java
+++ java/com/project/UsERP/controller/AndroidController.java
@@ -1,5 +1,6 @@
 package com.project.UsERP.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.UsERP.service.AndroidService;
+import com.project.UsERP.vo.EmployeeVO;
+import com.project.UsERP.vo.SalaryVO;
+import com.project.UsERP.vo.StockSupplyVO;
+import com.project.UsERP.vo.StockVO;
 
 @Controller
 public class AndroidController {
@@ -31,5 +36,72 @@ public class AndroidController {
 		
 		return map;
 	}
+	
+	// 김민수 - 안드로이드 재고수불부
+	@ResponseBody
+	@RequestMapping("android/androidSupply")
+	public List<StockSupplyVO> androidSupply(HttpServletRequest req) {
+		logger.info("url : 안드로이드 재고 수불부");
+		
+//		List<StockSupplyVO> supplyvo = service.androidSupply(req);
+		
+		return service.androidSupply(req);
+	}
+	
+	// 최유성 - 재고현황
+   @ResponseBody
+   @RequestMapping("android/androidStock")
+   public List<StockVO> androidStock(HttpServletRequest req) {
+	      logger.info("url: 안드로이드 재고현황");
+	      
+	      List<StockVO> stoList = service.androidStock(req);
+	      
+	      return stoList;
+   }
+   
+   // 김은희 - 인사카드조회
+   @ResponseBody
+   @RequestMapping("android/androidEmployee")
+   public List<EmployeeVO> androidEmployee(HttpServletRequest req) {
+      logger.info("url: 안드로이드 인사카드조회");
+      
+      List<EmployeeVO> empList = service.androidEmployee(req);
+      
+      return empList;
+   }
+   
+	// 조명재 - 급여내역조회
+   @ResponseBody
+   @RequestMapping("android/androidSalary")
+   public List<SalaryVO> androidSalary(HttpServletRequest req) {
+      logger.info("url: 안드로이드 급여내역");
+      
+      List<SalaryVO> salList = service.androidSalary(req);
+      
+      return salList;
+   }
+   
+   // 강재현 - 재무상태표
+   @ResponseBody
+   @RequestMapping("android/androidBS")
+   public Map<String, String> androidBS(HttpServletRequest req) {
+      logger.info("url: 안드로이드 재무상태표");
+      
+      Map<String, String> map = service.androidBS(req);
+      
+      return map;
+   }
+   
+   // 이재홍 - 손익계산서
+   @ResponseBody
+   @RequestMapping("android/androidIS")
+   public Map<String, String> androidIS(HttpServletRequest req) {
+      logger.info("url: 안드로이드 손익계산서");
+      
+      Map<String, String> map = service.androidIS(req);
+      
+      return map;
+   }
+
 	
 }
--- java/com/project/UsERP/service/AndroidServiceImpl.java
+++ java/com/project/UsERP/service/AndroidServiceImpl.java
@@ -1,6 +1,7 @@
 package com.project.UsERP.service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Service;
 
 import com.project.UsERP.persistence.AndroidDAO;
 import com.project.UsERP.vo.EmployeeVO;
+import com.project.UsERP.vo.SalaryVO;
+import com.project.UsERP.vo.StockSupplyVO;
+import com.project.UsERP.vo.StockVO;
 
 @Service
 public class AndroidServiceImpl implements AndroidService {
@@ -44,5 +48,84 @@ public class AndroidServiceImpl implements AndroidService {
 		
 		return map;
 	}
+
+	// 김민수 - 안드로이드 재고수불부
+	@Override
+	public List<StockSupplyVO> androidSupply(HttpServletRequest req) {
+		List<StockSupplyVO> supplyvo = dao.getSupplyList();
+		
+		return supplyvo;
+	}
+	
+	// 최유성 - 재고현황
+   @Override
+   public List<StockVO> androidStock(HttpServletRequest req) {
+      
+      List<StockVO> stoList = dao.getStoInfo();
+      
+      return stoList;
+   }
+
+   // 김은희  - 인사카드
+   @Override
+   public List<EmployeeVO> androidEmployee(HttpServletRequest req) {
+      
+      List<EmployeeVO> empList = dao.getEmployeeInfo();
+      
+      return empList;
+   }
+   
+   // 조명재 - 급여내역
+   @Override
+   public List<SalaryVO> androidSalary(HttpServletRequest req) {
+      
+      List<SalaryVO> salList = dao.getSalInfo();
+      
+      return salList;
+   }
+   
+   // 강재현 - 재무상태표
+   @Override
+   public Map<String, String> androidBS(HttpServletRequest req) {
+      
+      String get1 = String.valueOf(dao.get1()); //재무상태표 상품
+      String get2 = String.valueOf(dao.get2()); //재무상태표 외상매출금
+      String get3 = String.valueOf(dao.get3()); //
+      String get4 = String.valueOf(dao.get4()); //재무상태표 외상매입금
+
+      // 웹에서 안드로이드로 전달할 값
+      Map<String, String> map = new HashMap<String, String>();
+      
+      map.put("get1",get1); //재무상태표 상품
+      map.put("get2",get2); //재무상태표 외상매출금
+      map.put("get3",get3);
+      map.put("get4",get4); //재무상태표 외상매입금
+      
+      return map;
+   }
+
+   // 이재홍 - 손익계산서
+   @Override
+   public Map<String, String> androidIS(HttpServletRequest req) {
+      
+      String sum = String.valueOf(dao.sum()); //매출액
+      String sum1 = String.valueOf(dao.sum1()); //매출원가
+      String sum2 = String.valueOf(dao.sum2()); //판매비와 관리비
+      //매출 총이익
+      String sum3 = String.valueOf(dao.sum() - dao.sum1());
+      //영업 이익
+      String sum4 = String.valueOf(dao.sum() - dao.sum1() - dao.sum2());
+      
+      // 웹에서 안드로이드로 전달할 값
+      Map<String, String> map = new HashMap<String, String>();
+      
+      map.put("sum",sum); // 손익계산서 매출액
+      map.put("sum1",sum1); // 손익계산서 매출원가
+      map.put("sum2",sum2); // 손익계산서 판매비와 관리비
+      map.put("sum3",sum3); // 손익계산서 매출 총이익
+      map.put("sum4",sum4); // 손익계산서 영업 이익
+      
+      return map;
+   }
 	
 }
